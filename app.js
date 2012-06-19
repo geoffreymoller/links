b6ac683c38843789e2840547fae43dbff9b018eb
Geoff Moller <geoff.moller@gmail.com>
2012-06-18 18:18:17 -0700

    express

---
+++ app.js
@@ -0,0 +1,39 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , port;
+
+var app = module.exports = express.createServer();
+
+// Configuration
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  port = 3000;
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  port = 80;
+  app.use(express.errorHandler());
+});
+
+// Routes
+
+app.get('/', routes.index);
+
+port = process.env.PORT || port;
+app.listen(port);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
