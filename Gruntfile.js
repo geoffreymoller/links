0d7773a736f4eb231e26789692da231c64749621
Geoff Moller <geoff.moller@gmail.com>
2013-04-28 18:16:03 -0700

    cleanup gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,22 +20,4 @@ module.exports = function (grunt) {
     testacular.server.start({configFile:'testacular.conf.js'});
   });
 
-  grunt.registerTask('test', 'run tests', function () {
-
-    var testCmd = process.platform === 'win32' ? 'testacular.cmd' : 'testacular';
-    var testArgs = process.env.TRAVIS ? ['start', '--single-run', '--no-auto-watch', '--reporter=dots', '--browsers=Firefox'] : ['run'];
-
-    var specDone = this.async();
-    var child = grunt.utils.spawn({cmd:testCmd, args:testArgs}, function (err, result, code) {
-      if (code) {
-        grunt.fail.fatal("Test failed...", code);
-      } else {
-        specDone();
-      }
-    });
-
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-  });
-
 };
